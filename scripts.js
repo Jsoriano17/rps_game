0d1c3f672bfa3bf2dc81aa9721956c52c4fe6af7
Jeffrey Soriano <jeffsor17@gmail.com>
2020-03-02 17:04:35 -0700

    main functionality

--- scripts.js
+++ scripts.js
@@ -8,26 +8,76 @@ a reset can be done using the reset button */
 
 //rps game
 let score = 0;
-let player_Score = 0;
-let computer_Score = 0;
+let playerScore = 0;
+let computerScore = 0;
+const rock = document.querySelector('.r')
+const paper = document.querySelector('.p')
+const scissors = document.querySelector('.s')
+const playerScoreBoard = document.getElementById('player')
+const computerScoreBoard = document.getElementById('computer')
+
+function computerChoice() {
+let rpsArr = ['r', 'p', 's'];
+let randomNumber = Math.floor(Math.random() * 3);
+return rpsArr[randomNumber];
+}
+
+function win() {
+    playerScore++;
+    playerScoreBoard.innerHTML = playerScore;
+    console.log(win);
+}
+
+function draw() {
+    console.log(draw);
+}
+
+function lose() {
+    computerScore++;
+    computerScoreBoard.innerHTML = computerScore;
+    console.log(lose);
+
+}
 
 
+function game(userChoice) {
+    const getComputerChoice = computerChoice();
+    switch (userChoice + getComputerChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+           win();
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+          lose();
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw();
+            break;
+    }
+};
+
+function main() {
+
+    rockChoice = rock.addEventListener('click', function() {
+        game('r')
+    });
+
+    paperChoice = paper.addEventListener('click', function() {
+        game('p')
+    });
+
+    scissorsChoice = scissors.addEventListener('click', function() {
+        game('s')
+    });
+  };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main();
 
 
 //button hover
@@ -44,3 +94,17 @@ buttonClick.addEventListener('mouseout', function () {
     button.style.display = 'block';
 })
 
+//reset
+button.addEventListener('click', function () {
+    playerScore = 0;
+    computerScore = 0;
+    let playerScoreBoard = document.getElementById('player').innerHTML = 0;
+    let computerScoreBoard = document.getElementById('computer').innerHTML = 0;
+});
+
+buttonClick.addEventListener('click', function () {
+    playerScore = 0;
+    computerScore = 0;
+    let playerScoreBoard = document.getElementById('player').innerHTML = 0;
+    let computerScoreBoard = document.getElementById('computer').innerHTML = 0;
+})
